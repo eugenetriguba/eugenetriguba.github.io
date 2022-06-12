458ee37239c31a36c4b7a73a3d3e06bbcf77656c
Eugene Triguba <eugenetriguba@gmail.com>
2022-06-11 20:56:47 -0500

    Initial Commit

---
+++ pelicanconf.py
@@ -0,0 +1,23 @@
+AUTHOR = 'Eugene Triguba'
+SITENAME = 'Eugene Triguba'
+SITEURL = 'https://eugenetriguba.com'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+FILENAME_METADATA = '(?P<title>.*)'
+
+DEFAULT_LANG = 'en'
+THEME = 'simple'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
